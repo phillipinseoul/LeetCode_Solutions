872d36f957f27392e98afb7cb9d9447873dbf9bf
phillip9807@gmail.com <phillip9807@gmail.com>
2020-12-30 14:27:14 +0900

    first commit

---
+++ longest_substr.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std; 
+
+
+int lengthOfLongestSubstring(string s) {
+        
+    int maxlen = 0;
+    
+    for (int i = 0; i < s.length(); i++)
+    {
+        int j = i, sublen = 1;
+        set<char> ch;
+        ch.insert(s.at(j));
+                
+        // Extend substring until a repeating character appears
+        while ((++j) < s.length() && ch.find(s.at(j)) == ch.end())
+        {
+            ch.insert(s.at(j));
+            sublen++;
+        }
+            
+        if (sublen > maxlen)
+        {
+            maxlen = sublen;    
+        }
+    }
+    return maxlen;
+}
